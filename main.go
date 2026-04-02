3f8bc4a56957c5919d381d4964df3d8a1e8f8c22
Suryansh Shukla <suryanshshukla23@lpu.in>
2026-04-02 11:41:02 +0530

    Made a Health checker handler function

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
+	"goshort/internal/handler"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Server is running :)",
-		})
-	})
+	r.GET("/", handler.HealthCheck)
 	r.Run(":8080")
 }
